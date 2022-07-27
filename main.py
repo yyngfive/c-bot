147a2d8dd090fbed1251f6a4e2afe1d0ce2eb5fc
yyngfive <chenhye5@outlook.com>
2022-07-28 01:56:24 +0800

    基本复刻了之前用官方bot sdk实现的功能，但是没有ark或者embed消息

--- main.py
+++ main.py
@@ -9,13 +9,33 @@ appid = config['bot']['appid']
 token = config['bot']['token']
 bot = AmiyaBot(appid=appid, token=token,private=True)
 
+bot.prefix_keywords = ['cbot', 'c-bot']
 
 @bot.on_message(keywords='/hello')
 async def hello(data: Message):
     return Chain(data).text(f'hello, {data.nickname}')
 
 @bot.on_message(keywords='/help')
-async def _(data: Message):
-    return Chain(data).text(f'help, {data.nickname}')
+async def help(data: Message):
+    
+    return Chain(data,at=False).text(
+'''欢迎使用c-bot，你可以使用cbot或c-bot+/指令名使用\n
+/hello:打招呼\n
+/help:查看帮助\n
+/me:查看自己信息\n
+/luck:生成0~100随机数\n
+''')
+
+@bot.on_message(keywords='/me')
+async def me(data: Message):
+    nickname = data.nickname
+    user_id = data.user_id
+    joined_at = data.joined_at[0:10]
+    avatar = data.avatar
+    return Chain(data).text(f'你好，{nickname}，你的id为{user_id}，你加入频道的时间是{joined_at}，你的头像为：').image(target='',url=avatar)
+
+@bot.on_message(keywords='/luck')
+async def luck(data: Message):
+    return Chain(data).text(f'你的当前运势：{randint(0,100)}')
 
 asyncio.run(bot.start())
