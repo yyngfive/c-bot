4921c81ecef3430ba86f02058c2823709c612cb9
yyngfive <chenhye5@outlook.com>
2022-07-28 01:07:35 +0800

    基本搭建

--- main.py
+++ main.py
@@ -1,13 +1,21 @@
 import asyncio
-
+from random import randint
+import yaml
 from amiyabot import AmiyaBot, Message, Chain
-
-bot = AmiyaBot(appid='102018850', token='rrCvFLmWDQ9LRrxbZLxEaFCqQM6sDl3r',private=True)
+# 此版本中日志功能可能存在中文编码问题，需在log.py手动添加utf-8
+with open('./config.yml') as f:
+    config = yaml.load(f, yaml.FullLoader)
+appid = config['bot']['appid']
+token = config['bot']['token']
+bot = AmiyaBot(appid=appid, token=token,private=True)
 
 
 @bot.on_message(keywords='/hello')
-async def _(data: Message):
+async def hello(data: Message):
     return Chain(data).text(f'hello, {data.nickname}')
 
+@bot.on_message(keywords='/help')
+async def _(data: Message):
+    return Chain(data).text(f'help, {data.nickname}')
 
 asyncio.run(bot.start())
