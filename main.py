f8241b5384adbedaf5a4150a0a10edcda7271f09
yyngfive <chenhye5@outlook.com>
2022-08-16 17:19:47 +0800

    1. 完成打卡签到功能 2. 功能的增加通过配置文件

--- main.py
+++ main.py
@@ -5,30 +5,34 @@ from amiyabot import AmiyaBot, Message, Chain
 from utils import local,config
 
 config_bot = config.read_config('./config/config.yml')
+config_func = config.read_config('./config/functions.yml')
+
 appid = config_bot.login.appid
 token = config_bot.login.token
 bot = AmiyaBot(appid=appid, token=token, private=True)
 bot.prefix_keywords = config_bot.prefix
 
 
-@bot.on_message(keywords='/hello')
+@bot.on_message(keywords=config_func.hello.key)
 async def hello(data: Message):
     return Chain(data).text(f'hello, {data.nickname}')
 
 
-@bot.on_message(keywords='/help')
+@bot.on_message(keywords=config_func.help.key)
 async def help(data: Message):
 
-    return Chain(data, at=False).text(
-        '''欢迎使用c-bot，你可以使用cbot或c-bot+/指令名使用\n
-/hello:打招呼\n
-/help:查看帮助\n
-/me:查看自己信息\n
-/luck:生成0~100随机数\n
-''')
+    help_info = [f'欢迎使用c-bot，你可以使用{"或".join(config_bot.prefix)}+/指令名使用']
+    for fun in config_func.values():
+        if type(fun.key) is list:
+            help_info.append('或'.join(fun.key) + ': ' + fun.desc)
+        else:
+            help_info.append(fun.key + ': ' + fun.desc)
+
+    return Chain(data, at=False).text('\n'.join(help_info))
 
 
-@bot.on_message(keywords='/me')
+
+@bot.on_message(keywords=config_func.me.key)
 async def me(data: Message):
     nickname = data.nickname
     user_id = data.user_id
@@ -37,12 +41,12 @@ async def me(data: Message):
     return Chain(data).text(f'你好，{nickname}，你的id为{user_id}，你加入频道的时间是{joined_at}，你的头像为：').image(target='', url=avatar)
 
 
-@bot.on_message(keywords='/luck')
+@bot.on_message(keywords=config_func.luck.key)
 async def luck(data: Message):
     return Chain(data).text(f'你的当前运势：{randint(0,100)}')
 
 
-@bot.on_message(keywords='/morning')
+@bot.on_message(keywords=config_func.morning.key)
 async def morning(data: Message):
     morning, created = db.Morning.get_or_create(user=str(data.user_id))
     if morning.checkin_date == local.today():
--- utils/config.py
+++ utils/config.py
@@ -2,7 +2,7 @@ import yaml
 from munch import DefaultMunch
 
 
-def read_config(path: str) -> object:
+def read_config(path: str) -> DefaultMunch:
     with open(path, encoding='utf-8') as f:
         config = yaml.safe_load(f)
     return DefaultMunch.fromDict(config)
