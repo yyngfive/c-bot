b5add89e934335120ba481fe2e1c72fa03ca57d2
yyngfive <chenhye5@outlook.com>
2022-08-16 14:24:58 +0800

    1. 建立模组目录：database，utils 2. 修改机器人配置文件 3. 增加functions配置文件

--- main.py
+++ main.py
@@ -1,32 +1,36 @@
 import asyncio
 from random import randint
 import yaml
-import aiohttp
-import database as db
+import database.database as db
 from amiyabot import AmiyaBot, Message, Chain
-# 此版本中日志功能可能存在中文编码问题，需在log.py手动添加utf-8
-with open('./config.yml') as f:
-    config = yaml.load(f, yaml.FullLoader)
-appid = config['bot']['appid']
-token = config['bot']['token']
-bot = AmiyaBot(appid=appid, token=token,private=True)
+from munch import DefaultMunch
+import utils
 
-bot.prefix_keywords = ['cbot', 'c-bot']
+with open('./config/config.yml', encoding='utf-8') as f:
+    config = yaml.safe_load(f)
+    config_bot = DefaultMunch.fromDict(config)
+appid = config_bot.login.appid
+token = config_bot.login.token
+bot = AmiyaBot(appid=appid, token=token, private=True)
+bot.prefix_keywords = config_bot.prefix
+
 
 @bot.on_message(keywords='/hello')
 async def hello(data: Message):
     return Chain(data).text(f'hello, {data.nickname}')
 
+
 @bot.on_message(keywords='/help')
 async def help(data: Message):
-    
-    return Chain(data,at=False).text(
-'''欢迎使用c-bot，你可以使用cbot或c-bot+/指令名使用\n
+
+    return Chain(data, at=False).text(
+        '''欢迎使用c-bot，你可以使用cbot或c-bot+/指令名使用\n
 /hello:打招呼\n
 /help:查看帮助\n
 /me:查看自己信息\n
 /luck:生成0~100随机数\n
 ''')
+
 
 @bot.on_message(keywords='/me')
 async def me(data: Message):
@@ -34,17 +38,22 @@ async def me(data: Message):
     user_id = data.user_id
     joined_at = data.joined_at[0:10]
     avatar = data.avatar
-    return Chain(data).text(f'你好，{nickname}，你的id为{user_id}，你加入频道的时间是{joined_at}，你的头像为：').image(target='',url=avatar)
+    return Chain(data).text(f'你好，{nickname}，你的id为{user_id}，你加入频道的时间是{joined_at}，你的头像为：').image(target='', url=avatar)
+
 
 @bot.on_message(keywords='/luck')
 async def luck(data: Message):
     return Chain(data).text(f'你的当前运势：{randint(0,100)}')
 
+
 @bot.on_message(keywords='/morning')
 async def morning(data: Message):
-    morning,created = db.Morning.get_or_create(user = str(data.user_id))
+    morning, created = db.Morning.get_or_create(user=str(data.user_id))
     morning.checkin = morning.checkin + 1
     morning.save()
     return Chain(data).text(f'签到成功！已累计签到{morning.checkin}天')
 
-asyncio.run(bot.start())
+try:
+    asyncio.run(bot.start())
+except KeyboardInterrupt:
+    ...
